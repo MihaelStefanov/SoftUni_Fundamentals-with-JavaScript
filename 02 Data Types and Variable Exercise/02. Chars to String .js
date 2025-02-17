15f66cf14ded410f8fc6c38419d5bc1cb19c40ed
MihaelStefanov <145503588+MihaelStefanov@users.noreply.github.com>
2025-02-17 17:42:39 +0200

    Delete 02 Data Types and Variable Exercise directory

--- 02 Data Types and Variable Exercise/02. Chars to String .js
+++
@@ -1,6 +0,0 @@
-function solve(a, b, c)  {
-    console.log(`${c + b + a}\n${c.charCodeAt(0)} ${b.charCodeAt(0)} ${a.charCodeAt(0)}`)
-
-}
-    
-solve('1', '5', 'p');
--- 02 Data Types and Variable Exercise/07. Lower or Upper .js
+++
@@ -1,7 +0,0 @@
-function solve(char) {
-    let res = char == char.toUpperCase();
-    console.log(res? 'upper-case' : 'lower-case');
-
-}
-
-solve('M');
