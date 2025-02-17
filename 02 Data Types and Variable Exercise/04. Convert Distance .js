1b492c2b74f8861434fa50380903877d4303f2bf
MihaelStefanov <145503588+MihaelStefanov@users.noreply.github.com>
2025-02-17 17:48:36 +0200

    Delete 02 Data Types and Variable Exercise directory

--- 02 Data Types and Variable Exercise/04. Convert Distance .js
+++
@@ -1,8 +0,0 @@
-function solve(meters) {
-    let kilometers = meters / 1000;
-    let miles = kilometers * 0.621371;
-    console.log(`${meters} meters is equal to ${kilometers} kilometers.\n${kilometers} kilometers is equal to ${miles.toFixed(2)} miles.`);
-
-}
-
-solve(798);
