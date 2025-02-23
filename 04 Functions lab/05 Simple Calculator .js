28b550d413800179facd1ac6542f1a5d91513995
MihaelStefanov <145503588+MihaelStefanov@users.noreply.github.com>
2025-02-23 23:46:04 +0200

    Add files via upload

---
+++ 04 Functions lab/05 Simple Calculator .js
@@ -0,0 +1,18 @@
+function simpleCalculator(numOne, numTwo, operator) {
+
+    let add = (a, b) => a + b;
+    let subtract = (a, b) => a - b;
+    let multiply = (a, b) => a * b;
+    let divide = (a, b) => a / b;
+
+    switch (operator) {
+        case "add": return add(numOne, numTwo);
+        case "subtract": return subtract(numOne, numTwo);
+        case "multiply": return multiply(numOne, numTwo);
+        case "divide": return divide(numOne, numTwo);
+    }
+    
+
+}
+
+console.log(simpleCalculator(5, 5, 'multiply'));
