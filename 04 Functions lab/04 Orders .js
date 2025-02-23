9b332df98ddd8180b62dc7243b131aebd245a2d1
MihaelStefanov <145503588+MihaelStefanov@users.noreply.github.com>
2025-02-24 00:59:39 +0200

    Add files via upload

---
+++ 04 Functions lab/04 Orders .js
@@ -0,0 +1,20 @@
+function orders(product, qty) {
+    product = product.toLowerCase();
+
+    let productObj = {
+        water : 1.00,
+        coffee : 1.50,
+        coke : 1.40,
+        snacks : 2.00,
+        redbull : 3.50
+    }
+
+    let calc = (product, qty) => (productObj[product] * qty).toFixed(2);
+
+    return calc(product, qty);
+
+}
+
+console.log(orders("Water", 5));
+console.log(orders("CoFfEe", 2));
+console.log(orders("RedBull", 2));
