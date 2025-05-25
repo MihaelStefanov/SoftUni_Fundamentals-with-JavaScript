007e158273c9065569db7d1a7e060a20837295ce
Mihael <145503588+MihaelStefanov@users.noreply.github.com>
2025-05-25 14:21:11 +0300

    Add files via upload

---
+++ 08 Text Processing Lab/02. Substring .js
@@ -0,0 +1,7 @@
+function solve(string, startIdx, count) {
+    let result = string.substring(startIdx, startIdx + count);
+    console.log(result);
+}
+
+solve('ASentence', 1, 8);
+solve('SkipWord', 4, 7);
