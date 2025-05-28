cc9b657baeb9c1e45dfd2af7ba2d453e82832e6f
Mihael <145503588+MihaelStefanov@users.noreply.github.com>
2025-05-28 15:09:55 +0300

    Add files via upload

---
+++ 08. Text Processing Exercise/04. String Substring .js
@@ -0,0 +1,14 @@
+function solve(word, sentence) {
+    sentence = sentence.toLowerCase().split(' ');
+    
+
+    if (sentence.includes(word)) {
+        console.log(word);
+    } else {
+        console.log(`${word} not found!`);
+        
+    }
+
+}
+
+solve('javascript', 'JavaScript is the best programming language');
